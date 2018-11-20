c55c729e6a1b407ffee2f99f54c81323965c412c
virtualdesigner <30769960+virtualdesigner@users.noreply.github.com>
2018-11-20 21:55:02 +0530

    Start implement skills list page.

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -5,7 +5,7 @@ export default class SkillsList extends React.Component {
   render() {
     return (
       <div
-        id={this.props.upOrDown}
+        id="skills-list"
         style={{
           transitionProperty: "margin",
           transitionDuration: "1s",
@@ -13,7 +13,15 @@ export default class SkillsList extends React.Component {
           marginTop: this.props.currentDiv > 2 ? "-100vh" : "0vh"
         }}
       >
-        <div id="about-my-skills" />
+        <div id="skills-list__head">
+          <p id="skills-list__heading">THINGS I KNOW WELL</p>
+          <div id="skills-list__heading-underline">2</div>
+          <p id="skills-list__secondary-heading">
+            Skills that i have acquired while on the joy of learning
+          </p>
+        </div>
+
+        <div id="skills-list__skills" />
       </div>
     );
   }
